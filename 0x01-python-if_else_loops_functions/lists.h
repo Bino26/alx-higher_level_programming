156be0e77fe12a13d5693e6c66e7da14ee7e8ec9
Bino26 <hbino1999@gmail.com>
2022-05-31 10:11:28 -0700

    Trying Check

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,13 +1,14 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *
+ * 
  */
 typedef struct listint_s
 {
@@ -23,3 +24,4 @@ listint_t *insert_node(listint_t **head, int number);
 
 #endif /* LISTS_H */
 
+    
